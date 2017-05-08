a8ae50076549cc2ec56180223089d56c9be8a905
Nickpess <nickswaggins41801@gmail.com>
2017-05-08 09:11:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+<body>
+<style>
+        <script>if (confirm("Welcome to Fun National Parks!") == true) {
+    txt = "You pressed OK!";
+} else {
+    txt = "You pressed Cancel!";
+}
+<script/>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+<body>
